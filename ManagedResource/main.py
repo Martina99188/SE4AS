462c5da842880a520e5b533d54d00a383bb54a72
micheleintrevado <michele.intrevado.pc@gmail.com>
2023-01-24 15:07:23 +0100

    prove cambio mode

--- ManagedResource/main.py
+++ ManagedResource/main.py
@@ -13,12 +13,12 @@ def main():
     # room creation
     rooms = []
 
-    bath_room = Room(roomName="bathRoom",  light=140, temperature=22, humidity=50, movement=1)
-    rooms.append(bath_room)
-    kitchen = Room(roomName="kitchen", light=150, temperature=20, humidity=50, movement=0)
-    rooms.append(kitchen)
-    bedroom = Room(roomName="bedRoom",  light=140, temperature=22, humidity=50, movement=1)
-    rooms.append(bedroom)
+    #bath_room = Room(roomName="bathRoom",  light=140, temperature=22, humidity=50, movement=1)
+    #rooms.append(bath_room)
+    #kitchen = Room(roomName="kitchen", light=150, temperature=20, humidity=50, movement=0)
+    #rooms.append(kitchen)
+    #bedroom = Room(roomName="bedRoom",  light=140, temperature=22, humidity=50, movement=1)
+    #rooms.append(bedroom)
     living_room = Room(roomName="livingRoom", light=180, temperature=27, humidity=48, movement=0)
     rooms.append(living_room)
 
